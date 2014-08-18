188eb80ff808cb4315e3d061387a4304b22d92b1
douwevdijk <douwevdijk@hotmail.com>
2014-08-18 07:54:47 +0200

    1st commit

---
+++ js/locations-autocomplete.js
@@ -0,0 +1,5 @@
+var locations = [
+{ value: "Amsterdam - Tweede Oosterparkstraat 154-R"},
+{ value: "Utrecht - Stationsplein 97-Duvenborch, 3511 ED Utrecht"},
+{ value: "Amsterdam - Brouwersgracht 12"}
+];
